763a70585efa9f0cc45009980812ffd78a7e9c09
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 21:28:00 +0530

    Exp-6-B-For-Loop-Even-No.cpp

---
+++ Exp-6-B-For-Loop-Even-No.cpp
@@ -0,0 +1,18 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i; // variable for using in loop
+    // Print even numbers
+    for (i = 2; i <= 10; i += 2) {
+        cout << i << " ";
+    }
+    return 0;
+}
+/*
+Output : 
+2 4 6 8 10 
+*/
