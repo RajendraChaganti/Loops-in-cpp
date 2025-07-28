0e2d5cd8640597ca88e245ee6d137a6b6aa67e05
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 21:31:05 +0530

    Exp-6-C-For-Loop-Print-SIT-5-Times.cpp

---
+++ Exp-6-C-For-Loop-Print-SIT-5-Times.cpp
@@ -0,0 +1,23 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i; // loop variable
+    // Print "SIT" 5 times
+    for (i = 1; i <= 5; i++) {
+        cout << "SIT" << endl;
+    }
+    return 0;
+}
+/*
+Output :
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
+
