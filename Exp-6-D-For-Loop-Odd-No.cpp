6ff674f3cd34c9e69fe743dbf0c950546021fa3c
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 21:34:35 +0530

    Exp-6-D-For-Loop-Odd-No.cpp

---
+++ Exp-6-D-For-Loop-Odd-No.cpp
@@ -0,0 +1,18 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i; // loop variable
+    // for odd numbers 
+    for (i = 1; i < 10; i += 2) {
+        cout << i << " ";
+    }
+    return 0;
+}
+/*
+Output :
+1 3 5 7 9 
+*/
