73c71563261001e5ea2944c80376e51f41ee4250
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 21:47:10 +0530

    Exp-6-G-Nested-For-Loop-EquilateralTriangle.cpp

---
+++ Exp-6-G-Nested-For-Loop-EquilateralTriangle.cpp
@@ -0,0 +1,28 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i, j;
+    for (i = 1; i <= 6; i++) {
+        for (j = 1; j <= 6 - i; j++) {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+Output :
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+* * * * * * 
+*/
