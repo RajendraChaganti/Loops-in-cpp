049f9c6b3c6bacce3a013d32bad1cf4f4edbb6c5
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 22:07:45 +0530

    Exp-6-J-For-Loop-PrintNumbersUpto20.cpp

---
+++ Exp-6-J-For-Loop-PrintNumbersUpto20.cpp
@@ -0,0 +1,17 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    for (i = 1; i <= 20; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
+/*
+Output :
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
+*/
