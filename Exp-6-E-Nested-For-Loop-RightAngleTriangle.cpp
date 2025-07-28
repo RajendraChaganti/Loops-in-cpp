1c4eb60e7b35d4a95f5299589586ba60fd89794f
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 21:38:30 +0530

    Exp-6-E-Nested-For-Loop-RightAngleTriangle.cpp

---
+++ Exp-6-E-Nested-For-Loop-RightAngleTriangle.cpp
@@ -0,0 +1,25 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i, j;
+    for (i = 1; i <= 6; i++) {
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+Output :
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * *
+*/
