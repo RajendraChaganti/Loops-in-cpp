8964b11293523a78e4897119961c621dc30613ba
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-28 22:03:34 +0530

    Exp-6-I-For-Loop-RightAngledTriangleOfIncreasingNumbers.cpp

---
+++ Exp-6-I-For-Loop-RightAngledTriangleOfIncreasingNumbers.cpp
@@ -0,0 +1,24 @@
+/*Rajendra Chaganti
+24070123150
+B3
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int i, j, num = 1;
+    for (i = 1; i <= 4; i++) {
+        for (j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+Output :
+1 
+2 3 
+4 5 6 
+7 8 9 10
+*/
